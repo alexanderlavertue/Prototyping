b6c8598e22cbc739ed7300f9d6ffc6782a9984bf
alexanderlavertue <77847631+alexanderlavertue@users.noreply.github.com>
2021-03-28 15:08:53 -0400

    commit

--- script.js
+++ script.js
@@ -11,12 +11,13 @@ var colors = ['#760CE8', '#4782B1', '#E8890C'];
 var font = ['4px','5px','10px', '12px', '6px', '15px', '13px', '18px', '22px', '3px'] 
 var button = document.getElementById ('button2');
 var numbers = ['5rem','10rem','15rem', '20rem', '25rem', '30rem', '35rem', '40rem', '45rem', '50rem'] 
+var numberstwo = ['5rem','6rem','7rem', '8rem', '9rem', '10rem', '11rem', '12rem', '13rem', '14rem'] 
 button.onclick = function (){
 document.body.style.background = colors[Math.floor(Math.random()*colors.length)];
 document.getElementById('text').style.fontSize = font[Math.floor((Math.random() * 10) + 1)];
 imgs.forEach((img, i)=>{
    img.style.left = numbers[Math.floor((Math.random() * 10) + 1)];
-   img.style.bottom = numbers[Math.floor((Math.random() * 10) + 1)];
+   img.style.bottom = numberstwo[Math.floor((Math.random() * 10) + 1)];
 })
 };
 //Makes images positon random on page load
@@ -24,6 +25,6 @@ var body = document.querySelector("body")
 body.onload = function(){
    imgs.forEach((img, i)=>{
       img.style.left = numbers[Math.floor((Math.random() * 10) + 1)];
-      img.style.bottom = numbers[Math.floor((Math.random() * 10) + 1)];
+      img.style.bottom = numberstwo[Math.floor((Math.random() * 10) + 1)];
    })
 };
