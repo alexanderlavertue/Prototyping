8d356c9985cf9b0507d2eab2d44bd309812254a8
alexanderlavertue <77847631+alexanderlavertue@users.noreply.github.com>
2021-03-28 15:00:28 -0400

    pro

--- script.js
+++ script.js
@@ -6,16 +6,24 @@ test.onclick = function myfunction() {
       img.style.opacity = "1";
    })
 };
-// changes font size. 
+// changes font size/backround color/images position on remix button push
 var colors = ['#760CE8', '#4782B1', '#E8890C'];
-var font = ['4px','5px,','10px', '12px', '6px', '15px', '13px', '18px', '22px', '3px'] 
+var font = ['4px','5px','10px', '12px', '6px', '15px', '13px', '18px', '22px', '3px'] 
 var button = document.getElementById ('button2');
-var numbers = ['5rem','10rem,','15rem', '20rem', '25rem', '30rem', '35rem', '40rem', '45rem', '50rem'] 
-button.onclick = function myfunctiontwo(){
+var numbers = ['5rem','10rem','15rem', '20rem', '25rem', '30rem', '35rem', '40rem', '45rem', '50rem'] 
+button.onclick = function (){
 document.body.style.background = colors[Math.floor(Math.random()*colors.length)];
-document.getElementById ('text').style.fontSize = font[Math.floor((Math.random() * 10) + 1)];
+document.getElementById('text').style.fontSize = font[Math.floor((Math.random() * 10) + 1)];
 imgs.forEach((img, i)=>{
-    img.style.left += numbers[Math.floor((Math.random() * 10) + 1)]; 
-    img.style.bottom += numbers[Math.floor((Math.random() * 10) + 1)]; 
+   img.style.left = numbers[Math.floor((Math.random() * 10) + 1)];
+   img.style.bottom = numbers[Math.floor((Math.random() * 10) + 1)];
 })
+};
+//Makes images positon random on page load
+var body = document.querySelector("body")
+body.onload = function(){
+   imgs.forEach((img, i)=>{
+      img.style.left = numbers[Math.floor((Math.random() * 10) + 1)];
+      img.style.bottom = numbers[Math.floor((Math.random() * 10) + 1)];
+   })
 };
